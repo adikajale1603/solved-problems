b19805ca638106afcfa8047b55a12148c2638413
adikajale1603 <65041519+adikajale1603@users.noreply.github.com>
2022-06-12 09:36:02 +0530

    Time: 468 ms (55.65%), Space: 103.7 MB (59.54%) - LeetHub

---
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& v) {
+          
+        int last=-1;
+        int n=v.size();
+        
+        unordered_map<int,int>ump;
+        int sum=0;
+        vector<int>prefix(n+1);
+        
+        for(int i=0;i<n;i++){
+            prefix[i+1]=v[i]+prefix[i];
+        }
+        
+        for(int i=0;i<n;i++){
+            
+            if(ump.count(v[i])){
+               last=max(last,ump[v[i]]);
+            }
+            ump[v[i]]=i;
+            sum=max(sum,prefix[i+1]-prefix[last+1]);
+        }
+        return sum;
+    }
+};

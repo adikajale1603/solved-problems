721cda9187ddf962579cc721a49e4880f955cc92
adikajale1603 <65041519+adikajale1603@users.noreply.github.com>
2022-06-13 12:56:06 +0530

    Time: 7 ms (75.60%), Space: 8.7 MB (55.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& v) {
+    
          
         
         
-        int n=v.size();
-        vector<vector<int>>dp(2,vector<int>(n,INT_MAX));
-        
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                for(int k=0;k<2;k++){
-                    if(i==0 && j==0){
-                        dp[i%2][j]=v[i][j];
-                        continue;
-                    }
-                    if(j==i && k==0){
-                        continue;
-                    }
-                    dp[i%2][j]=min(k==0?INT_MAX:dp[i%2][j],(j-k>=0?v[i][j]+dp[(i-1)%2][j-k]:INT_MAX));
-                   
-                }
-            }
+      int minimumTotal(vector<vector<int> > &triangle) {
+    int n = triangle.size();
+    vector<int> minlen(triangle.back());
+    for (int layer = n-2; layer >= 0; layer--) // For each layer
+    {
+        for (int i = 0; i <= layer; i++) // Check its every 'node'
+        {
+            // Find the lesser of its two children, and sum the current value in the triangle with it.
+            minlen[i] = min(minlen[i], minlen[i+1]) + triangle[layer][i]; 
         }
-        
-        return *min_element(dp[(n-1)%2].begin(),dp[(n-1)%2].end());
-        
-        
     }
+    return minlen[0];
+}
+        
+        
+
 };

becc35d6522288bb9043d19dd24e3c4bb811d1a6
adikajale1603 <65041519+adikajale1603@users.noreply.github.com>
2022-06-16 01:00:58 +0530

    Time: 555 ms (8.67%), Space: 48.8 MB (12.54%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -3,75 +3,33 @@ public:
     static bool cmp(string &s1,string &s2){
         return s1.size()<s2.size();
     }
-    void dfs(string src,map<string,vector<string>>&V,map<string,int>&vis,map<string,int>&dp,int &lph){
-        vis[src]=1;
-        dp[src]=0;
-        for(auto u:V[src]){
-            if(!vis[u]){
-                dfs(u,V,vis,dp,lph);
-                dp[src]=max(dp[src],1+dp[u]);
-                lph=max(dp[src],lph);
-            }
-        }
-    }
+
     int longestStrChain(vector<string>& s) {
            
         int n=s.size();
         sort(s.begin(),s.end(),cmp);
-        map<string,vector<string>>v;
-        map<string,int>vis;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(abs((int)s[i].size()-(int)s[j].size())==1){
-                map<char,int>mp1;
-                map<char,int>mp2;
-                int cnt=0;
-                int n=s[i].size(),m=s[j].size();
-                int k=0,p=0;
-                for(;p<max(n,m)&&k<min(n,m);k++,p++){
-                    if(n<m){
-                        if(s[i][k]!=s[j][p]){
-                            cnt++;
-                            k--;
-                        }
-                    }else{
-                        cout<<"yes";
-                        if(s[i][p]!=s[j][k]){
-                            cnt++;
-                            k--;
-                        }
-                    }
-                }
-                cout<<cnt<<"\n";
-                if(cnt<=1){
-                    if((int)s[i].size()>int(s[j].size())){
-                        v[s[j]].push_back(s[i]);
-                    }else{
-                    v[s[i]].push_back(s[j]);
-                    }  
-                }
-                }
-            }
-        }
-        for(int i=0;i<n;i++){
-            cout<<s[i]<<" ->";
-            for(auto x:v[s[i]]){
-                cout<<x<<" ";
-            }
-            cout<<"\n";
-        }
         int ans=0;
+        
+        map<string,int>dp;
+       
         for(int i=0;i<n;i++){
-            int cnt=0;
-            if(!vis[s[i]]){
-                map<string,int>dp;
-                dfs(s[i],v,vis,dp,cnt);
-                ans=max(cnt+1,ans);
-                
+            
+            for(int j=0;j<s[i].size();j++){
+                string s1="";
+                for(int k=0;k<=j-1;k++){
+                    s1.push_back(s[i][k]);
+                }
+               
+                for(int k=j+1;k<s[i].size();k++){
+                    s1.push_back(s[i][k]);
+                }
+                dp[s[i]]=max(dp[s[i]],1+dp[s1]);
             }
+            ans=max(ans,dp[s[i]]);
         }
         
-        return ans;
+      return ans;
+        
         
     }
 };

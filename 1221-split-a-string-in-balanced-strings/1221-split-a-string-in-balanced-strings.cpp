46b002ea8870b21918363b821579811cf8474ca4
adikajale1603 <65041519+adikajale1603@users.noreply.github.com>
2022-06-17 18:05:10 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (80.25%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -4,19 +4,19 @@ public:
         
         int n=s.size();
         
-       vector<int>L(n+1);
-       vector<int>R(n+1);
-        
-        for(int i=0;i<n;i++){
-            L[i+1]=L[i]+(s[i]=='L');
-            R[i+1]=R[i]+(s[i]=='R');
-        }
         int cnt=0;
-        for(int i=1;i<=n;i++){
-            if(L[i]==R[i] && (L[n]-L[i])==(R[n]-R[i])){
-                cnt++;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            cnt+=(s[i]=='R');
+            cnt-=(s[i]=='L');
+            if(cnt==0){
+                ans++;
             }
         }
-        return cnt;
+        if(cnt==0){
+            return ans;
+        }else{
+            return 0;
+        }
     }
 };

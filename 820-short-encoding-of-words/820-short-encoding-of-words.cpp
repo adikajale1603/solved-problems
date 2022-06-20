e065d1ae0d64b74aa26b45d55523f33c72798f44
adikajale1603 <65041519+adikajale1603@users.noreply.github.com>
2022-06-20 14:07:00 +0530

    Time: 218 ms (15.85%), Space: 71.8 MB (36.14%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,12 +1,22 @@
+class TrieNode {
+public:
+    unordered_map<char, TrieNode *> next;
+};
 class Solution {
 public:
-      int minimumLengthEncoding(vector<string>& words) {
-        unordered_set<string> s(words.begin(), words.end());
-        for (string w : s)
-            for (int i = 1; i < w.size(); ++i)
-                s.erase(w.substr(i));
+    int minimumLengthEncoding(vector<string>& words) {
+        TrieNode *root = new TrieNode;
+        vector<pair<TrieNode *, int>> leaves;
+        for (auto & w : unordered_set<string> (words.begin(), words.end())) {
+            TrieNode *cur = root;
+            for (int i = w.length() - 1; i >= 0; --i) {
+                if (cur->next.count(w[i]) == 0) cur->next[w[i]] = new TrieNode;
+                cur = cur->next[w[i]];
+            }
+            leaves.push_back(make_pair(cur, w.length() + 1));
+        }
         int res = 0;
-        for (string w : s) res += w.size() + 1;
+        for (auto leaf : leaves) if ((leaf.first->next).size() == 0) res += leaf.second;
         return res;
     }
 };

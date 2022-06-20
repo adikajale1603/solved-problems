5df8642c00a2a77eb75953e3d732f05041b642b9
adikajale1603 <65041519+adikajale1603@users.noreply.github.com>
2022-06-20 13:52:14 +0530

    Time: 326 ms (8.92%), Space: 36.5 MB (50.00%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumLengthEncoding(vector<string>& words) {
+           
+        
+        sort(words.begin(), words.end(), [](const string &l, const string &r) { return l.size() > r.size(); });
+        
+        map<string,int>mp;
+        
+        int n=words.size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            if(!mp.count(words[i])){
+                ans+=(words[i].size()+1);
+                for(int j=0;j<words[i].size();j++){
+                    string temp="";
+                    for(int k=j;k<words[i].size();k++){
+                       temp.push_back(words[i][k]);
+                    }
+                    mp[temp]=1;
+                }
+            }
+        }
+        return ans;
+          
+    }
+};

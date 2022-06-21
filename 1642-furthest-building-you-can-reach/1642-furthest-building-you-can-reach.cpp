ff2cca4534bd0e85f9509ba5f54bcdfbed262861
adikajale1603 <65041519+adikajale1603@users.noreply.github.com>
2022-06-22 01:40:28 +0530

    Time: 186 ms (29.53%), Space: 53.6 MB (77.33%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
-            
-          
-            int n=heights.size();
-            
-            vector<int>v;
-            
-            int l=0,r=n-1;
-            int ans=0;
-            while(l<=r){
-                int mid=(l+r)>>1;
-             
-                vector<int>v;
-                
-                for(int i=0;i<=min((mid-1),n-2);i++){
-                    if(heights[i+1]-heights[i]>0)
-                    v.push_back(heights[i+1]-heights[i]);
-                }
-                sort(v.begin(),v.end());
-                   int sum=0;
-                if(v.size()-ladders>=0){
-                for(int i=0;i<(int(v.size())-ladders);i++){
-                    sum+=v[i];
-                }
-                }
-                if(sum<=bricks){
-                    ans=mid;
-                    l=mid+1;
-                }else{
-                    r=mid-1;
-                }
-                
-            }
+
+              
         
-        return ans;
            
-    } 
+                 priority_queue<int>pq;
+        
+           int n=heights.size();
+        
+          for(int i=0;i<n-1;i++){
+              
+              
+              int d=heights[i+1]-heights[i];
+              
+              if(d>0){
+                  pq.push(-d);
+              }
+              
+              if(pq.size()>ladders){
+                  bricks+=pq.top();
+                  pq.pop();
+              }
+              
+              if(bricks<0){
+                  return i;
+              }
+          }
+        
+        
+        return n-1;
+          
+           
+            
+
+    }
 };
